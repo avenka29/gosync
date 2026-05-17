2e67b84f7e1dfcfbd924d9d58a86d4b968de17e0
Adith Venkatesh <avenkatesh081@gmail.com>
2026-05-16 22:13:36 -0400

    modified test files to show broadcasting effects.

--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+	"math/rand"
 	"github.com/avenka29/gosync"
 )
 
 func main() {
 	server := gosync.NewServer()
 
-	// 1. Start a goroutine to handle the Passive Pipe
+	// 1. Handle incoming events from the pipe
 	go func() {
-		log.Println("Gosync Event Processor started...")
 		for ctx := range server.Events() {
 			event := ctx.Event
-			
-			log.Printf("Received Event: %s", event.Name)
-
-			// 2. Logic: If it's a chat message, broadcast it to everyone
 			if event.Name == "chat" {
-				// We just pass the raw data through
 				server.BroadcastEvent("chat", event.Data)
 			}
 		}
 	}()
 
+	// 2. Periodically broadcast a "color-change" event every 3 seconds
+	go func() {
+		colors := []string{"#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff"}
+		ticker := time.NewTicker(3 * time.Second)
+		
+		for range ticker.C {
+			randomColor := colors[rand.Intn(len(colors))]
+			log.Printf("Broadcasting new color: %s", randomColor)
+			
+			// We send the "color-change" event to all connected browsers
+			server.BroadcastEvent("color-change", randomColor)
+		}
+	}()
+
 	http.Handle("/ws", server)
 
-	log.Println("Gosync Server running on :8080")
+	log.Println("Gosync Color-Changer Server running on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
